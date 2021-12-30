7aa093aba5b28b69f6812905954c00488e057cf2
A.K.M <adithmurali@protonmail.com>
2021-12-30 13:43:50 +0530

    Update liquid.cpp

--- src/liquid.cpp
+++ src/liquid.cpp
@@ -1,8 +1,29 @@
+#include <GLFW/glfw3.h>
 #include <iostream>
 using namespace std;
 
-int main()
+int main(int argc, const char** argv)
 {
-    cout<<"Hello, world!";
+    GLFWwindow* window;
+
+    if(!glfwInit())
+    {
+        cout<<"Couldn't initialize GLFW!"<<endl;
+        return 1;
+    }
+
+    window = glfwCreateWindow(640, 480, "Liquid Media Player", NULL, NULL);
+    if(!window)
+    {
+        cout<<"Couldn't create window!"<<endl;
+        return 1;
+    }
+
+    glfwMakeContextCurrent(window);
+    while(!glfwWindowShouldClose(window))
+    {
+        glfwWaitEvents();
+    }
+
     return 0;
 }
