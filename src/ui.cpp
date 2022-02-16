7ad3a8255ae5da42f2677ba6468ceb2b7ae7daef
itsr1ght <bjlegend007@gmail.com>
2022-02-16 18:32:00 +0530

    Added vloume slider and some improvements

--- src/ui.cpp
+++ src/ui.cpp
@@ -6,6 +6,8 @@
 
 
 int test;
+int soundvar;
+bool stay = false;
 
 void init_imgui(SDL_Window* window, SDL_Renderer* renderer)
 {
@@ -17,7 +19,6 @@ void init_imgui(SDL_Window* window, SDL_Renderer* renderer)
 
     // Setup Dear ImGui style
     ImGui::StyleColorsDark();
-
     // Setup Platform/Renderer bindings
     // window is the SDL_Window*
     // context is the SDL_GLContext
@@ -38,29 +39,63 @@ void update_imgui(SDL_Renderer* renderer)
     flags |= !ImGuiWindowFlags_MenuBar;
     flags |= ImGuiWindowFlags_NoResize;
 
+    //imgui child window flags
+    ImGuiWindowFlags child_window_flags = 0;
+    child_window_flags |= ImGuiWindowFlags_NoTitleBar;
+    child_window_flags |= !ImGuiWindowFlags_MenuBar;
+    child_window_flags |= ImGuiWindowFlags_NoResize;
+    child_window_flags |= ImGuiWindowFlags_NoMove;
+
+
+    //imgui style flags
+    ImGuiStyle& style = ImGui::GetStyle();
+    style.WindowRounding = 12;
+    style.ChildRounding = 12;
+    style.TabRounding = 12;
+    style.FrameRounding = 12;
+    style.GrabRounding = 12;
+
+    ImVec2 win_pos;
     //render definition Here
-    //ImGui::ShowDemoWindow();
+    //ImGui::ShowDemoWindow(); //uncomment to show imgui demo window
     ImGui::Begin("Window",(bool *)true,flags);
+    win_pos = ImGui::GetWindowPos();
+    win_pos.x += 120;
+    win_pos.y -= 110;
+    ImGui::NewLine();
+    ImGui::NewLine();
+    ImGui::SameLine((ImGui::GetWindowWidth()*10)/100);
+    ImGui::PushItemWidth((ImGui::GetWindowWidth()*80)/100);
+    ImGui::SliderInt(" ",&test,10,100);
+    ImGui::NewLine();
 
-    ImGui::NewLine();
-    ImGui::NewLine();
     ImGui::SameLine((ImGui::GetWindowWidth()*15)/100);
-    ImGui::SliderInt("time",&test,10,100);
-    ImGui::NewLine();
+    if(ImGui::Button("S",{35,35}))
+    {
+        stay = !stay;
+    }
+    if(stay)
+    {
+        ImGui::Begin("sound_window",nullptr,child_window_flags);
+        ImGui::SetWindowPos(win_pos);
+        ImGui::PushItemWidth((ImGui::GetWindowWidth()*90)/100);
+        ImGui::VSliderInt("##int",{20,80},&soundvar,0,100);
+        ImGui::End();
+    }
 
-    ImGui::SameLine((ImGui::GetWindowWidth()*15)/100);
-    ImGui::Button("sound",{35,35});
+    ImGui::SameLine((ImGui::GetWindowWidth()*42)/100);
+    if(ImGui::Button("<",{35,35}))
+    {
 
-    ImGui::SameLine((ImGui::GetWindowWidth()*40)/100);
-    ImGui::Button("<",{35,35});
+    }
 
-    ImGui::SameLine((ImGui::GetWindowWidth()*45)/100);
-    if(ImGui::Button("play",{35,35}))
+    ImGui::SameLine((ImGui::GetWindowWidth()*47.5)/100);
+    if(ImGui::Button("P",{35,35}))
     {
         //toggle_pause(state);  gives error(compiler problem)
     }
 
-    ImGui::SameLine((ImGui::GetWindowWidth()*50)/100);
+    ImGui::SameLine((ImGui::GetWindowWidth()*53)/100);
     if(ImGui::Button(">",{35,35}))
     {
 
